a4884cb91e865a3e1121b46af1e07612409da9df
Ethernaljoz <155591690+Ethernaljoz@users.noreply.github.com>
2024-12-06 14:39:30 -0800

    auth middleware error fix

--- backend/src/utils/Types.ts
+++ backend/src/utils/Types.ts
@@ -1,6 +1,15 @@
+import mongoose from "mongoose";
+
 export const enum verificationCodeType {
     EmailVerification = "email_verification",
     PasswordReset = "password_reset"
 }
 
-
+declare global {
+    namespace Express{
+        interface Request{
+            userId: mongoose.Types.ObjectId
+            sessionId: mongoose.Types.ObjectId 
+        }
+    }
+}
